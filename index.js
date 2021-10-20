e66ad4e1c4680685dbcb287659a251738c4c0178
kalanimojica <mojica.kalani@gmail.com>
2021-10-19 17:23:05 -0700

    employee loop until user is finished

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ const Manager = require("./lib/Manager");
 
 //Node
 const inquirer = require("inquirer");
-const fs = require("fs");
+//const fs = require("fs");
 
 //team array
-const teamArr= [];
-
+const managerArr= [];
+const engineerArr= [];
+const internArr= [];
 //Manager
-const addManager = () => { 
+function addManager() { 
     return inquirer.prompt([
     {
         type: "input",
@@ -34,19 +35,112 @@ const addManager = () => {
         message: "Enter your office number.",
         name: "officeNum",
     },
-    {
-        type: "list",
-        message: "What would you like to do?",
-        name: "choices",
-        choices: ["Add Engineer", "Add Intern", "Finish Building Team"]
-    },
     ])
     .then((newManager) => {
-        const {name, id, email, officeNumber} = newManager;
-        const manager = new Manager (name, id, email, officeNumber); 
-        teamArr.push(manager); 
+        const {name, id, email, officeNum} = newManager;
+        const manager = new Manager (name, id, email, officeNum); 
+        managerArr.push(manager); 
         console.log(manager);
         
+        return getEmployee(); 
         });
+        
+};
 
+function getEmployee() { 
+    return inquirer.prompt([ 
+        { 
+            type: "list",
+            message: "What would you like to do?",
+            name:"next", 
+            choices: ["Add Engineer", "Add Intern", "Finish Building Team"],
+        }, 
+    ])
+    .then((answers) => { 
+        switch(answers.next){ 
+            case "Add Engineer": 
+            return addEngineer(); 
+
+            case "Add Intern": 
+            return addIntern(); 
+
+            default: 
+            return renderListing(); 
+        }
+    })
 }
+
+//function to add engineer
+function addEngineer() { 
+    return inquirer.prompt([
+        { 
+           type: "input", 
+           message: "Enter your name: ", 
+           name: "name",
+        }, 
+        { 
+            type: "input", 
+            message: "Enter your employee id: ", 
+            name: "id",
+         }, 
+         { 
+            type: "input", 
+            message: "Enter your email: ", 
+            name: "email",
+         }, 
+         { 
+            type: "input", 
+            message: "Enter your github username: ", 
+            name: "github",
+         }, 
+    ])
+    .then((newEngineer) => {
+        const {name, id, email, github} = newEngineer;
+        const engineer = new Engineer (name, id, email, github); 
+        engineerArr.push(engineer); 
+        console.log(engineer);
+        console.log(engineerArr);
+
+        return getEmployee(); 
+        });
+}
+
+function addIntern() { 
+    return inquirer.prompt([
+        { 
+           type: "input", 
+           message: "Enter your name: ", 
+           name: "name",
+        }, 
+        { 
+            type: "input", 
+            message: "Enter your employee id: ", 
+            name: "id",
+         }, 
+         { 
+            type: "input", 
+            message: "Enter your email: ", 
+            name: "email",
+         }, 
+         { 
+            type: "input", 
+            message: "Enter your school: ", 
+            name: "school",
+         }, 
+    ])
+    .then((newIntern) => {
+        const {name, id, email, school} = newIntern;
+        const intern = new Intern (name, id, email, school); 
+        internArr.push(intern); 
+        console.log(intern);
+        console.log(internArr);
+
+        return getEmployee(); 
+        });
+}
+
+function renderListing() { 
+    const text = "render HTML for listings"; 
+    return fs.writeFile("build/team.html", text);
+}
+addManager();
